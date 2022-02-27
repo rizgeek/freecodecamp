2294542311c7a99aaa99fd4ec96f95c31b6e191a
Muhammad Rizki <rizki@madeindonesia.com>
2022-02-27 13:02:48 +0700

    Arithmetic Formatter

---
+++ arithmetic-formatter/main.py
@@ -0,0 +1,10 @@
+from arithmetic_arranger import arithmetic_arranger
+from unittest import main
+
+
+# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
